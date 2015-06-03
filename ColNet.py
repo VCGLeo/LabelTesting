94ae0b611228f1786b2c081426970711dd8e0e4e
Shiroid <shadowbowser@hotmail.com>
2015-06-03 12:07:39 +0200

    Added data structure for colored nets

---
+++ ColNet.py
@@ -0,0 +1,36 @@
+###
+# @file ColNet.py
+# @author Leo van Gansewinkel (l.w.p.h.v.gansewinkel@student.tue.nl)
+#
+# Class representing a colored node and edge network
+###
+import collections
+
+class ColNet:
+    def __init__(self):
+        self.nodeCols = []          # Array of node colors
+        self.edges = []             # Array of edges
+        self.edgeCols = []          # Array of edge colors
+
+    def transform(self):
+        pass
+
+    def addNode(self, col):
+        self.nodeCols.append(col)
+
+    def addEdge(self, source, target, col):
+        self.edgeCols.append(col)
+        self.edgeCols.append((source,target))
+
+    #Returns this network in .col format
+    def __str__(self):
+        nodes = self.nodeCols
+        edges = self.edges
+        result = "p edge " + str(len(nodes)) + " " + str(len(edges))
+        for i in xrange(1, len(nodes)+1):
+            if nodes[i-1] > 0:
+                result +=  "\n v " + i + " " + nodes[i-1]
+        for i in xrange(1, len(edges)+1):
+            result +=  "\n e " + edges[i-1][0] + " " + edges[i-1][1]
+
+        return rep
